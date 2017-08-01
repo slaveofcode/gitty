6e402194eeddb12c8aaa38cfae169394bbc80e66
Aditya <zeandcode@gmail.com>
2017-08-01 10:24:37 +0700

    Added clone capability

--- gitty/__init__.py
+++ gitty/__init__.py
@@ -1,12 +1,17 @@
 from dulwich.repo import Repo
+from dulwich import porcelain
 from .config import Config
 
 
 class Gitty:
-  repository = None
+    repository = None
   
-  def __init__(self, repo_path):
-    self.repository = Repo(repo_path)
+    def __init__(self, repo_path):
+      self.repository = Repo(repo_path)
 
-  def config(self, config_param):
-    return Config.get(self.repository, config_param)
+    def config(self, config_param):
+      return Config.get(self.repository, config_param)
+
+    @staticmethod
+    def clone(repo_link, local_folder='.'):
+        porcelain.clone(repo_link, local_folder)
